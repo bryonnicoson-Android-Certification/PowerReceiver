e92fb2d99077326ad129732781c204ad3d9541c3
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-08 10:42:51 -0600

    custom broadcast and receiver

--- app/src/main/java/com/bryonnicoson/powerreceiver/CustomReceiver.java
+++ app/src/main/java/com/bryonnicoson/powerreceiver/CustomReceiver.java
@@ -3,13 +3,33 @@ package com.bryonnicoson.powerreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class CustomReceiver extends BroadcastReceiver {
 
+    private static final String ACTION_CUSTOM_BROADCAST =
+            "com.bryonnicoson.powerreceiver.ACTION_CUSTOM_BROADCAST";
+
+    public CustomReceiver() {
+        
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        String intentAction = intent.getAction();
+        String toastMessage = null;
+
+        switch (intentAction){
+            case Intent.ACTION_POWER_CONNECTED:
+                toastMessage = context.getString(R.string.power_connected);
+                break;
+            case Intent.ACTION_POWER_DISCONNECTED:
+                toastMessage = context.getString(R.string.power_disconnected);
+                break;
+            case ACTION_CUSTOM_BROADCAST:
+                toastMessage = context.getString(R.string.custom_broadcast_toast);
+                break;
+        }
+        Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/bryonnicoson/powerreceiver/MainActivity.java
+++ app/src/main/java/com/bryonnicoson/powerreceiver/MainActivity.java
@@ -1,13 +1,58 @@
 package com.bryonnicoson.powerreceiver;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Parcelable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String ACTION_CUSTOM_BROADCAST =
+            "com.bryonnicoson.powerreceiver.ACTION_CUSTOM_BROADCAST";
+
+    private PackageManager mPackageManager;
+    private ComponentName mReceiverComponentName;
+    private CustomReceiver mCustomReceiver = new CustomReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mReceiverComponentName = new ComponentName(this, CustomReceiver.class);
+        mPackageManager = getPackageManager();
+
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mCustomReceiver, new IntentFilter(ACTION_CUSTOM_BROADCAST));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mPackageManager.setComponentEnabledSetting(mReceiverComponentName,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+    }
+
+    @Override
+    protected void onStop() {
+        mPackageManager.setComponentEnabledSetting(mReceiverComponentName,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mCustomReceiver);
+        super.onDestroy();
+    }
+
+    public void sendCustomBroadcast(View view) {
+        Intent customBroadCastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadCastIntent);
     }
 }
